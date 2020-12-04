c12e0cbb1f1ba1483faf1d1e8517aab8548dd7c2
KihiraLove <illuminatecmdc@gmail.com>
2020-12-04 17:06:03 +0100

    Project created
    Unity loaded
    Assets added for menu
    Menu and LevelSelect scenes added
    Scene changer script added

---
+++ Assets/ControllerScripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void changeLevel(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+    
+    public void exitLevel()
+    {
+        Application.Quit();
+    }
+}
